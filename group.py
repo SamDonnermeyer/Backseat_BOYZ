63e3a62a95c06a6e382d4e4e7df3b72b723cdf53
Sam Donnermeyer <donnermeyersam@gmail.com>
2021-09-15 14:21:55 -0600

    first commit

---
+++ group.py
@@ -0,0 +1,24 @@
+## Function for Group ##
+def group(name, course, semester):
+    print("Group Name: " & name)
+    print("Course: " & course)
+    print("Time: " & semester)
+
+### Functions for each Group Member ###
+def individual(name, year, major, hobbies, origin, interesting):
+    print("---\n")
+    print("Name: " & name)
+    print("Year in School: " & year)
+    print("Major: " & major)
+    print("Hobbies: " & hobbies)
+    print("Home Town: " & origin)
+    print("Interesting Facts: " & interesting)
+
+## Main ##
+if __name__ == '__main__':
+    ## Group ##
+    group("The Backseat Boyz", "CSCI 291", "Fall 2021")
+
+    ## Sam ##
+    individual("Sam", "Senior", "MIS", "Running, Rock Climbing, Hiking", "Mid-West", "I wear the same clothes")
+
